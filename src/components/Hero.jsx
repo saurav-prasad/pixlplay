15420de7bf6fe0c42510e7a081ba5ab9239c1244
saurav-prasad <sauravprasad2050@gmail.com>
2025-01-21 18:46:41 +0530

    testing

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -11,22 +11,22 @@ function Hero() {
       <div
         className={`relative h-screen flex justify-center flex-col md:text-center bg-[#ffffff00] backdrop-blur-[2px]`}
       >
-        <h1 class="mx-auto max-w-4xl font-display text-5xl font-medium tracking-tight text-slate-900 sm:text-7xl">
+        <h1 className="mx-auto max-w-4xl font-display text-5xl font-medium tracking-tight text-slate-900 sm:text-7xl">
           Start{" "}
-          <span class="relative whitespace-nowrap text-blue-600">
+          <span className="relative whitespace-nowrap text-blue-600">
             <svg
               aria-hidden="true"
               viewBox="0 0 418 42"
-              class="absolute left-0 top-2/3 h-[0.58em] w-full fill-blue-300/70"
+              className="absolute left-0 top-2/3 h-[0.58em] w-full fill-blue-300/70"
               preserveAspectRatio="none"
             >
               <path d="M203.371.916c-26.013-2.078-76.686 1.963-124.73 9.946L67.3 12.749C35.421 18.062 18.2 21.766 6.004 25.934 1.244 27.561.828 27.778.874 28.61c.07 1.214.828 1.121 9.595-1.176 9.072-2.377 17.15-3.92 39.246-7.496C123.565 7.986 157.869 4.492 195.942 5.046c7.461.108 19.25 1.696 19.17 2.582-.107 1.183-7.874 4.31-25.75 10.366-21.992 7.45-35.43 12.534-36.701 13.884-2.173 2.308-.202 4.407 4.442 4.734 2.654.187 3.263.157 15.593-.78 35.401-2.686 57.944-3.488 88.365-3.143 46.327.526 75.721 2.23 130.788 7.584 19.787 1.924 20.814 1.98 24.557 1.332l.066-.011c1.201-.203 1.53-1.825.399-2.335-2.911-1.31-4.893-1.604-22.048-3.261-57.509-5.556-87.871-7.36-132.059-7.842-23.239-.254-33.617-.116-50.627.674-11.629.54-42.371 2.494-46.696 2.967-2.359.259 8.133-3.625 26.504-9.81 23.239-7.825 27.934-10.149 28.304-14.005.417-4.348-3.529-6-16.878-7.066Z"></path>
             </svg>
-            <span class="relative">creating </span>
+            <span className="relative">creating </span>
           </span>
           someting phenominal.
         </h1>
-        <p class="mx-auto mt-6 max-w-2xl font-normal text-lg tracking-tight text-[#1d1c2d]">
+        <p className="mx-auto mt-6 max-w-2xl font-normal text-lg tracking-tight text-[#1d1c2d]">
           <span
             className="font-whisper font-extrabold text-[2.1rem] text-indigo-700
             "
@@ -37,9 +37,9 @@ function Hero() {
           working solo or collaborating with others, Pixl Play meets all your
           expectations with ease and efficiency.
         </p>
-        <div class="mt-10 flex justify-center items-center gap-x-6">
+        <div className="mt-10 flex justify-center items-center gap-x-6">
           <Link
-            class="group inline-flex items-center justify-center rounded-full py-3 px-5 font-medium focus:outline-none focus-visible:outline-2 shadow-2xl focus-visible:outline-offset-2 bg-[#0f172ad5] text-white backdrop-blur-[100px] hover:bg-slate-800 hover:text-slate-100 active:bg-slate-800 active:text-slate-300 focus-visible:outline-slate-900 group"
+            className="group inline-flex items-center justify-center rounded-full py-3 px-5 font-medium focus:outline-none focus-visible:outline-2 shadow-2xl focus-visible:outline-offset-2 bg-[#0f172ad5] text-white backdrop-blur-[100px] hover:bg-slate-800 hover:text-slate-100 active:bg-slate-800 active:text-slate-300 focus-visible:outline-slate-900 group"
             variant="solid"
             color="slate"
             to={user ? "/canvases" : "/signin"}
@@ -49,16 +49,16 @@ function Hero() {
           </Link>
         </div>
         {/* footer */}
-        <footer class="absolute bottom-0 left-0 right-0 flex flex-col items-center border-t border-slate-400/10 py-4 sm:flex-row-reverse sm:justify-between bg-[#00000078] backdrop-blur-[100px] shadow-lg">
-          <div class="flex gap-x-6">
+        <footer className="absolute bottom-0 left-0 right-0 flex flex-col items-center border-t border-slate-400/10 py-4 sm:flex-row-reverse sm:justify-between bg-[#00000078] backdrop-blur-[100px] shadow-lg">
+          <div className="flex gap-x-6">
             <Link
               target="__blank"
               to="https://www.linkedin.com/in/saurav-prasadd/"
-              class="group"
+              className="group"
               aria-label="TaxPal on X"
             >
               <svg
-                class="h-6 w-6 fill-slate-500 group-hover:fill-slate-700"
+                className="h-6 w-6 fill-slate-500 group-hover:fill-slate-700"
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 50 50"
                 width="50px"
@@ -70,11 +70,11 @@ function Hero() {
             <Link
               to="https://github.com/saurav-prasad"
               target="__blank"
-              class="group"
+              className="group"
               aria-label="TaxPal on GitHub"
             >
               <svg
-                class="h-6 w-6 fill-slate-500 group-hover:fill-slate-700"
+                className="h-6 w-6 fill-slate-500 group-hover:fill-slate-700"
                 aria-hidden="true"
                 viewBox="0 0 24 24"
               >
@@ -82,7 +82,7 @@ function Hero() {
               </svg>
             </Link>
           </div>
-          <p class="mt-2 text-sm text-slate-500 sm:mt-0">
+          <p className="mt-2 text-sm text-slate-500 sm:mt-0">
             Copyright © 2025 Saurav Prasad. All rights reserved.
           </p>
         </footer>
--- src/socket/socket.js
+++ src/socket/socket.js
@@ -1,8 +1,10 @@
 import { io } from 'socket.io-client'
 
-const ur = "http://192.168.29.83:8080"
-const url = import.meta.env.VITE_VERCEL_ENV_SOCKET_URL
+const url = "http://192.168.29.83:8080"
+const ur = import.meta.env.VITE_VERCEL_ENV_SOCKET_URL
 
-const socket = io(url)
+const socket = io(url, {
+    transports: ["websocket"]
+})
 
 export default socket
