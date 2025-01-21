4963fb8f3e9d95ba4e9dd9fe21e456fff3a31255
saurav-prasad <sauravprasad2050@gmail.com>
2025-01-21 13:15:59 +0530

    minor changes

--- src/socket/socket.js
+++ src/socket/socket.js
@@ -1,7 +1,7 @@
 import { io } from 'socket.io-client'
 
-const url = "http://192.168.29.83:8080"
-const ur = import.meta.env.VITE_VERCEL_ENV_SOCKET_URL
+const ur = "http://192.168.29.83:8080"
+const url = import.meta.env.VITE_VERCEL_ENV_SOCKET_URL
 
 const socket = io(url)
 
