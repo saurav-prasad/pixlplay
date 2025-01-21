ca50f6d2912f6f45dcc41ef806fdf9c2ad77318c
saurav-prasad <sauravprasad2050@gmail.com>
2025-01-21 18:52:02 +0530

    testing

--- src/socket/socket.js
+++ src/socket/socket.js
@@ -3,8 +3,6 @@ import { io } from 'socket.io-client'
 const ur = "http://192.168.29.83:8080"
 const url = import.meta.env.VITE_VERCEL_ENV_SOCKET_URL
 
-const socket = io(url, {
-    transports: ["websocket"]
-})
+const socket = io(url)
 
 export default socket
