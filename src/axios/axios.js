817db98c1bc02061575e4a1f1854aa58194de1df
saurav-prasad <sauravprasad2050@gmail.com>
2025-01-25 21:17:15 +0530

    testing

--- src/axios/axios.js
+++ src/axios/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // url
-const ur = "http://192.168.29.83:5000";
+const urll = "http://192.168.29.83:5000";
 const url = import.meta.env.VITE_VERCEL_ENV_SERVER_URL
 
 // axios instances
--- src/socket/socket.js
+++ src/socket/socket.js
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client'
 
-const ur = "http://192.168.29.83:8080"
+const urll = "http://192.168.29.83:8080"
 const url = import.meta.env.VITE_VERCEL_ENV_SOCKET_URL
 
 const socket = io(url);
